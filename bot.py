6305fe067d44480e8a783ec1bae04469d0b8fc9f
szaw44418-crypto <szaw44418@gmail.com>
2026-09-09 15:11:59 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,24 @@
 import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), SimpleHandler)
+    server.serve_forever()
+
+# Server ကို Background မှာ အလုပ်လုပ်ခိုင်းရန်
+server_thread = threading.Thread(target=run_server, daemon=True)
+server_thread.start()
+
+
+import os
 import time
 import ccxt
 import pandas as pd
